1c9ce0d62043e054f12257c1f2e66fae2a360b40
Pierrick Hauguel <pierrickhauguel@outlook.com>
2024-09-10 09:20:49 +0200

    add correction

--- app/countries/create.tsx
+++ app/countries/create.tsx
@@ -1,48 +1,73 @@
+import { useRouter } from "expo-router";
 import { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  KeyboardAvoidingView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 const ListCountriesScreen = () => {
   const [name, setName] = useState("");
   const [population, setPopulation] = useState("");
   const [region, setRegion] = useState("");
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
 
   const createCountry = () => {
     console.log(name, population, region);
-    fetch("https://crudcrud.com/api//countries", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name,
-        population,
-        region,
-      }),
+    setLoading(true);
+    fetch(
+      "https://crudcrud.com/api/726b4e8544b94b61a6235891cfb91fe3/countries",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          population,
+          region,
+        }),
+      }
+    ).then(() => {
+      setLoading(false);
+      router.back();
     });
   };
 
   return (
-    <View style={styles.mainContainer}>
-      <TextInput
-        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-        onChangeText={setName}
-        value={name}
-        placeholder="Name"
-      />
-      <TextInput
-        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-        onChangeText={setPopulation}
-        value={population}
-        placeholder="Population"
-      />
-      <TextInput
-        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-        onChangeText={setRegion}
-        value={region}
-        placeholder="Region"
-      />
-      <Button onPress={createCountry} title="Create" />
-    </View>
+    <KeyboardAvoidingView
+      style={{ flex: 1, flexDirection: "column", justifyContent: "center" }}
+      behavior="padding"
+      enabled
+      keyboardVerticalOffset={80}
+    >
+      <ScrollView>
+        <TextInput
+          style={{ height: 40, borderColor: "red", borderWidth: 1 }}
+          onChangeText={setName}
+          value={name}
+          placeholder="Name"
+        />
+        <TextInput
+          style={{ height: 40, borderColor: "red", borderWidth: 1 }}
+          onChangeText={setPopulation}
+          value={population}
+          placeholder="Population"
+        />
+        <TextInput
+          style={{ height: 40, borderColor: "red", borderWidth: 1 }}
+          onChangeText={setRegion}
+          value={region}
+          placeholder="Region"
+        />
+        <Button onPress={createCountry} title="Create" disabled={loading} />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
--- app/countries/index.tsx
+++ app/countries/index.tsx
@@ -1,11 +1,48 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import useFetch from "@/hooks/useFetch";
+import { Link, useNavigation } from "expo-router";
+import { useEffect } from "react";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+
+type Country = {
+  name: string;
+  population: string;
+  region: string;
+  _id: string;
+};
 
 const ListCountriesScreen = () => {
+  const { data, loading, error, refetch } = useFetch<Country[]>(
+    "https://crudcrud.com/api/726b4e8544b94b61a6235891cfb91fe3/countries"
+  );
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.addListener("focus", refetch);
+
+    return () => {
+      navigation.removeListener("focus", refetch);
+    };
+  }, []);
+
   return (
     <View style={styles.mainContainer}>
-      <Text>Bienvenue</Text>
+      <Text>Mes pays</Text>
       <Link href="/countries/create">Go to Home</Link>
+      {loading ? (
+        <ActivityIndicator></ActivityIndicator>
+      ) : (
+        data?.map((country) => (
+          <Text
+            key={country._id}
+            style={{
+              fontSize: 40,
+            }}
+          >
+            {country.name}
+          </Text>
+        ))
+      )}
     </View>
   );
 };
